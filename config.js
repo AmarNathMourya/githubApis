395ae15486a787478cf8b44501904ec5bfc2d71d
Amar Nath Mourya <mouryaamar8826@gmail.com>
2018-08-17 09:17:38 +0530

    Pushed code into master branch

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    PORT: '3000',
+    CLIENT_ID: '4e619d2557f20c7a5708',
+    CLIENT_SECRET: '5c0c79ab21f248c892d1483c37c5cb7ddb87c81a'
+};
+
+
+
+/*Client ID
+    4e619d2557f20c7a5708
+Client Secret
+    5c0c79ab21f248c892d1483c37c5cb7ddb87c81a*/
